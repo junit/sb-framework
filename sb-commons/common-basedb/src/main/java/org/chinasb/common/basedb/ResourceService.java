6b23afbb0e5fd966bdb542db354efbdd5d2ac462
junit <ethan@5k6.org>
2014-04-30 15:57:16 +0800

    update ResourceService comment

--- sb-commons/common-basedb/src/main/java/org/chinasb/common/basedb/ResourceService.java
+++ sb-commons/common-basedb/src/main/java/org/chinasb/common/basedb/ResourceService.java
@@ -10,65 +10,64 @@ import java.util.List;
 public interface ResourceService {
     /**
      * 通过唯一标识获得基础数据
-     * @param paramObject 唯一标识
-     * @param paramClass 基础数据的对象类
+     * @param id 唯一标识
+     * @param clazz 基础数据的对象类
      * @return
      */
-    public <T> T get(Object paramObject, Class<T> paramClass);
+    public <T> T get(Object id, Class<T> clazz);
 
     /**
      * 通过索引获得基础数据集合
-     * @param paramString 索引名称
-     * @param paramClass 基础数据的对象类
-     * @param paramVarArgs 索引值
+     * @param indexName 索引名称
+     * @param clazz 基础数据的对象类
+     * @param indexValues 索引值
      * @return
      */
-    public <T> List<T> listByIndex(String paramString, Class<T> paramClass, Object... paramVarArgs);
+    public <T> List<T> listByIndex(String indexName, Class<T> clazz, Object... indexValues);
 
     /**
      * 通过索引获得基础数据唯一标识集合
-     * @param paramString 索引名称
-     * @param paramClass 基础数据的对象类
-     * @param paramClass1 唯一标识的数据类型
-     * @param paramVarArgs 索引值
+     * @param indexName 索引名称
+     * @param clazz 基础数据的对象类
+     * @param pk 唯一标识的数据类型
+     * @param indexValues 索引值
      * @return
      */
-    public <T, PK> List<PK> listIdByIndex(String paramString, Class<T> paramClass,
-            Class<PK> paramClass1, Object... paramVarArgs);
+    public <T, PK> List<PK> listIdByIndex(String indexName, Class<T> clazz, Class<PK> pk,
+            Object... indexValues);
 
     /**
      * 通过索引获得基础数据的第一条记录
-     * @param paramString 索引名称
-     * @param paramClass 基础数据的对象类
-     * @param paramVarArgs 索引值
+     * @param indexName 索引名称
+     * @param clazz 基础数据的对象类
+     * @param indexValues 索引值
      * @return
      */
-    public <T> T getByUnique(String paramString, Class<T> paramClass, Object... paramVarArgs);
+    public <T> T getByUnique(String indexName, Class<T> clazz, Object... indexValues);
 
     /**
      * 获得全部基础数据集合
-     * @param paramClass 基础数据的对象类
+     * @param clazz 基础数据的对象类
      * @return
      */
-    public <T> Collection<T> listAll(Class<T> paramClass);
+    public <T> Collection<T> listAll(Class<T> clazz);
 
     /**
      * 增加基础数据到索引数据集合
-     * @param paramString 索引名称
-     * @param paramObject 唯一标识
-     * @param paramClass 基础数据的对象类
+     * @param indexName 索引名称
+     * @param id 唯一标识
+     * @param clazz 基础数据的对象类
      */
-    public <T> void addToIndex(String paramString, Object paramObject, Class<T> paramClass);
+    public <T> void addToIndex(String indexName, Object id, Class<T> clazz);
 
     /**
      * 增加基础数据到索引数据集合
-     * @param paramString 索引名称
-     * @param paramObject 唯一标识
-     * @param paramClass 基础数据的对象类
-     * @param paramVarArgs 索引值
+     * @param indexName 索引名称
+     * @param id 唯一标识
+     * @param clazz 基础数据的对象类
+     * @param indexValues 索引值
      */
-    public <T> void addToIndex(String paramString, Object paramObject, Class<T> paramClass,
-            Object... paramVarArgs);
+    public <T> void addToIndex(String indexName, Object id, Class<T> clazz, Object... indexValues);
 
     /**
      * 重新加载全部基础数据
